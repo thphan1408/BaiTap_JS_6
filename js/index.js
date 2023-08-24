2fca632b5cd32eacf9f70b9017caaf57e3a4d356
Thành Phan <th.phan1408@gmail.com>
2023-08-24 11:24:05 +0700

    new update

--- js/index.js
+++ js/index.js
@@ -1,33 +1,40 @@
 // tìm số nguyên dương nhỏ nhất sao cho: 1 + 2 + 3  + ... + n > 10000
-function findMinNumber(n) {
+function findMinNumber() {
+  let n = +document.getElementById("number1").value;
+  let div = document.querySelector(".result");
   let sum = 0;
   while (sum <= 10000) {
     n++;
     sum += n;
   }
-  console.log(n);
+  div.innerHTML = `<p style="font-size: 20px">Số nguyên dương nhỏ nhất sao cho: 1 + 2 + 3  + ... + n > 10000 là: <span style="color: red; font-size: 20px"> ${n} </span></p> `;
 }
-findMinNumber(0);
 
 // Viết chương trình nhập vào 2 số x, n tính tổng S(n) = x + x^2 + x^3 + ... + x^n (sử dụng vòng lặp và hàm)
-function sum(x, n) {
+function sum() {
+  let x = +document.getElementById("number2").value;
+  let n = +document.getElementById("number3").value;
+  let div = document.querySelector(".result2");
+
   let sum = 0;
   for (let i = 1; i <= n; i++) {
     sum += Math.pow(x, i);
   }
-  console.log(sum);
+  div.innerHTML = `<p style="font-size: 20px">Tổng S(n) = x + x^2 + x^3 + ... + x^n là: <span style="color: red; font-size: 20px"> ${sum} </span></p> `;
 }
-sum(2, 3);
 
 // Nhập vào n. Tính gia thừa 1*2*...n (sử dụng vòng lặp và hàm)
-function factorial(n) {
+function factorial() {
+  let n = +document.getElementById("number4").value;
+  let div = document.querySelector(".result3");
+
   let sum = 1;
   for (let i = 1; i <= n; i++) {
     sum *= i;
   }
   console.log(sum);
+  div.innerHTML = `<p style="font-size: 20px">Tổng 1*2*...n là: <span style="color: red; font-size: 20px"> ${sum} </span></p> `;
 }
-factorial(5);
 
 // Viết chương trình khi click vào button sẽ in ra 10 thẻ div. Nếu div nào vị trí chẵn thì background màu đỏ và lẻ thì background màu xanh
 document.querySelector("#btnClick").onclick = function () {
